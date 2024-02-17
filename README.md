c6b3546ddff52ad24140f7c8f8f7721e99af689a
Kedar Basutkar <krb1202@gmail.com>
2024-02-17 17:08:55 +0530

    add tests for slices

---
+++ README.md
@@ -0,0 +1,7 @@
+TODO modify later
+
+Feaures about testing:
+1. `go test`
+2. `go test bench=.`
+3. `go test -cover`
+4. example functions
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -3,12 +3,14 @@ package arraysandslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of varying size", func(t *testing.T) {
+		mySlice := []int{1,2,3}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(mySlice)
+		want := 6
 
-	if got != want {
-		t.Errorf("got %v want %v, numbers: %v", got, want, numbers)
-	}
+		if got !=  want {
+			t.Errorf("got %d want %d, received %v", got, want, mySlice)
+		}
+	})
 }
