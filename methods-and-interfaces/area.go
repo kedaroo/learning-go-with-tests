0a0fd450018fe5cec7bb97ac941630f8874f6b07
Kedar Basutkar <krb1202@gmail.com>
2024-02-18 00:32:50 +0530

    add methods and interfaces tests

---
+++ methods-and-interfaces/area.go
@@ -0,0 +1,33 @@
+package methodsandinterfaces
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
